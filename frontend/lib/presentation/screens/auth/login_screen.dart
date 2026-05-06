690429f93db5da1965868749ddd7492939a19841
MalikFajriansyah <malik04fajriansyah@gmail.com>
2026-05-06 14:33:53 +0700

    fix: resolve flutter import paths for build success

--- frontend/lib/presentation/screens/auth/login_screen.dart
+++ frontend/lib/presentation/screens/auth/login_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../../core/theme/app_theme.dart';
+import '../../../core/theme/app_theme.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
--- frontend/lib/presentation/screens/splash/splash_screen.dart
+++ frontend/lib/presentation/screens/splash/splash_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import '../../core/theme/app_theme.dart';
+import '../../../core/theme/app_theme.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
